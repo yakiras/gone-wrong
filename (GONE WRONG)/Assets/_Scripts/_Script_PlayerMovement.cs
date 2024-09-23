87204fc473dfa550ebc573fcf732146f7e9a1e0c
Yakira Sun <zhenzhen201303@gmail.com>
2024-09-22 20:23:39 -0600

    Gravity working yippee

--- (GONE WRONG)/Assets/_Scripts/_Script_PlayerMovement.cs
+++ (GONE WRONG)/Assets/_Scripts/_Script_PlayerMovement.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class _Script_PlayerMovement : MonoBehaviour
 {
     public float speed = 12f;
+    public float gravity = -9.81f;
     public CharacterController controller;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +24,20 @@ public class _Script_PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
-
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
