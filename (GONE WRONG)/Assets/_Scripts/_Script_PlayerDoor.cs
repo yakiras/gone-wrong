c27471fda5cb69244584eb7bff118760d7956334
Yakira Sun <zhenzhen201303@gmail.com>
2024-11-12 21:44:27 -0700

    jankier door fix

--- (GONE WRONG)/Assets/_Scripts/_Script_PlayerDoor.cs
+++ (GONE WRONG)/Assets/_Scripts/_Script_PlayerDoor.cs
@@ -29,11 +29,11 @@ public class _Script_PlayerDoor : MonoBehaviour
                     doorCollider = door.GetComponent<MeshCollider>();
                     doorCollider.enabled = false;
 
-                    bool opened = hinge.GetComponent<_Script_doorHinge>().isOpen;
-                    targetRotation = opened ? Quaternion.Euler(0, hinge.rotation.y, 0) : Quaternion.Euler(0, hinge.rotation.y + openAngle, 0);
+                    _Script_doorHinge script = hinge.GetComponent<_Script_doorHinge>();
+                    targetRotation = script.isOpen ? Quaternion.Euler(0, hinge.rotation.y + script.closeChange, 0) : Quaternion.Euler(0, hinge.rotation.y + script.openChange, 0);
 
                     isAnimating = true;
-                    hinge.GetComponent<_Script_doorHinge>().isOpen = !opened;
+                    hinge.GetComponent<_Script_doorHinge>().isOpen = !script.isOpen;
                 }
             }
         }
--- (GONE WRONG)/Assets/_Scripts/_Script_door.cs
+++ (GONE WRONG)/Assets/_Scripts/_Script_door.cs
@@ -5,6 +5,24 @@ using UnityEngine;
 public class _Script_doorHinge : MonoBehaviour
 {
 
-    public bool isOpen = false;
+    public bool version1 = true;
 
+    public bool isOpen = false;
+    public float openChange;
+    public float closeChange;
+
+    void Start()
+    {
+        if (version1)
+        {
+            openChange = 90f;
+            closeChange = 0f;
+        }
+        else
+        {
+            openChange = 0f;
+            closeChange = -90f;
+        }
+    }
+        
 }
