cae440c946ccd5ae63a59b1cc2b01863a3d6541d
floating-sky <122412802+floating-sky@users.noreply.github.com>
2024-11-13 02:04:09 -0700

    Added start menu

---
+++ (GONE WRONG)/Assets/_Scripts/_Script_MainMenuController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class _Script_MainMenuController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Floorplan Testing");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
