318b99100568525528ab8cd47ea7268dcc421694
Yakira Sun <zhenzhen201303@gmail.com>
2024-11-19 19:44:10 -0700

    Can open two doors at once - angle still bugged

--- (GONE WRONG)/Assets/_Scripts/_Script_PlayerDoor.cs
+++ (GONE WRONG)/Assets/_Scripts/_Script_PlayerDoor.cs
@@ -6,14 +6,10 @@ using UnityEngine;
 public class _Script_PlayerDoor : MonoBehaviour
 {
     public float maxReach = 5f;
-    public float rotateSpeed = 3f;
-    public float openAngle = 90f;
 
     [SerializeField] private Transform playerCameraTrans;
     [SerializeField] private LayerMask doorLayerMask;
-    private Transform hinge;
     private MeshCollider doorCollider;
-    private Quaternion targetRotation;
     private bool isAnimating = false;
 
     void Update()
@@ -24,31 +20,11 @@ public class _Script_PlayerDoor : MonoBehaviour
             {
                 if (raycastHit.collider.TryGetComponent(out Component door) && door.gameObject.CompareTag("Door"))
                 {
-                    hinge = door.transform.parent;
-
                     doorCollider = door.GetComponent<MeshCollider>();
                     doorCollider.enabled = false;
 
-                    /*_Script_doorHinge script = hinge.GetComponent<_Script_doorHinge>();
-                    targetRotation = script.isOpen ? Quaternion.Euler(0, hinge.rotation.y + script.closeChange, 0) : Quaternion.Euler(0, hinge.rotation.y + script.openChange, 0);
-
-                    isAnimating = true;
-                    hinge.GetComponent<_Script_doorHinge>().isOpen = !script.isOpen;*/
+                    door.transform.parent.GetComponent<_Script_doorHinge>().openClose();
                 }
-            }
-        }
-
-        if (isAnimating)
-        {
-            hinge.rotation = Quaternion.Slerp(hinge.rotation, targetRotation, Time.deltaTime * rotateSpeed);
-            Debug.Log(hinge.rotation.y);
-
-            // Stop animating if we've reached the target rotation
-            if (Quaternion.Angle(hinge.rotation, targetRotation) < 0.1f)
-            {
-                hinge.rotation = targetRotation;
-                doorCollider.enabled = true;
-                isAnimating = false;
             }
         }
     }
--- (GONE WRONG)/Assets/_Scripts/_Script_door.cs
+++ (GONE WRONG)/Assets/_Scripts/_Script_door.cs
@@ -4,11 +4,46 @@ using UnityEngine;
 
 public class _Script_doorHinge : MonoBehaviour
 {
+    public bool isOpen = false;
+    public float rotateSpeed = 3f;
+    public float openAngle = 90f;
 
+    private Transform hinge;
+    private bool isAnimating = false;
+    private Quaternion targetRotation;
 
-    void Start()
+    private void Start()
     {
-        
+        hinge = gameObject.transform;
     }
-        
+
+    void Update()
+    {
+        if (isAnimating)
+        {
+            hinge.rotation = Quaternion.Lerp(hinge.rotation, targetRotation, Time.deltaTime * rotateSpeed);
+            Debug.Log(hinge.rotation.y);
+
+            // Stop animating if we've reached the target rotation
+            if (Quaternion.Angle(hinge.rotation, targetRotation) < 0.1f)
+            {
+                hinge.rotation = targetRotation;
+                hinge.GetChild(0).GetComponent<MeshCollider>().enabled = true;
+                isAnimating = false;
+            }
+        }
+    }
+
+    public void openClose()
+    {
+        if (isAnimating)
+            return;
+
+        Debug.Log(hinge.parent.rotation.y);
+
+        targetRotation = isOpen ? Quaternion.Euler(0, hinge.parent.rotation.y, 0) : Quaternion.Euler(0, hinge.parent.rotation.y - openAngle, 0);
+
+        isAnimating = true;
+        isOpen = !isOpen;
+    }
 }
