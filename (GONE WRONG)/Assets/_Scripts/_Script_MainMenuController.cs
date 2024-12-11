b5f494478d2229dc5919fff64aee684f14181442
Yakira Sun <zhenzhen201303@gmail.com>
2024-12-11 00:58:42 -0700

    Fixed endscreen cursor

--- (GONE WRONG)/Assets/_Scripts/_Script_MainMenuController.cs
+++ (GONE WRONG)/Assets/_Scripts/_Script_MainMenuController.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class _Script_MainMenuController : MonoBehaviour
 {
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Floorplan Testing");
