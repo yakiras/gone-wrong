42ad1a6073fe7e4d39e1866bb3f33b32acdf2263
Yakira Sun <zhenzhen201303@gmail.com>
2024-12-14 17:50:39 -0700

    Blackout working

--- (GONE WRONG)/Assets/_Scripts/_Script_FlipSwitch.cs
+++ (GONE WRONG)/Assets/_Scripts/_Script_FlipSwitch.cs
@@ -2,10 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class FlipSwitch : MonoBehaviour
 {
     public float rotateSpeed = 0.03f;
+
+    public GameObject lampsFlr1;
+    public GameObject lampsFlr2;
+    public GameObject lampsFlr3;
+    public GameObject lampsSecurity;
+
+    public Material unlitShade;
 
     private bool isAnimating;
     private Quaternion targetRotation;
@@ -22,12 +30,6 @@ public class FlipSwitch : MonoBehaviour
         if (isAnimating)
         {
             transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotateSpeed);
-
-            // Stop animating if we've reached the target rotation
-            //if (Quaternion.Angle(transform.rotation, targetRotation) < 0.01f)
-            //{
-            //    transform.rotation = targetRotation;
-            //}
         }
     }
 
@@ -37,5 +39,37 @@ public class FlipSwitch : MonoBehaviour
             return;
 
         isAnimating = true;
+
+        foreach (Transform parent in lampsSecurity.transform)
+        {
+            Transform lamp = parent.GetChild(0);
+
+            lamp.GetChild(0).GetComponent<Renderer>().material = unlitShade;
+            lamp.GetChild(1).GetComponent<Light>().enabled = false;
+        }
+
+        foreach (Transform parent in lampsFlr3.transform)
+        {
+            Transform lamp = parent.GetChild(0);
+
+            lamp.GetChild(0).GetComponent<Renderer>().material = unlitShade;
+            lamp.GetChild(1).GetComponent<Light>().enabled = false;
+        }
+
+        foreach (Transform parent in lampsFlr2.transform)
+        {
+            Transform lamp = parent.GetChild(0);
+
+            lamp.GetChild(0).GetComponent<Renderer>().material = unlitShade;
+            lamp.GetChild(1).GetComponent<Light>().enabled = false;
+        }
+
+        foreach (Transform parent in lampsFlr1.transform)
+        {
+            Transform lamp = parent.GetChild(0);
+
+            lamp.GetChild(0).GetComponent<Renderer>().material = unlitShade;
+            lamp.GetChild(1).GetComponent<Light>().enabled = false;
+        }
     }
 }
