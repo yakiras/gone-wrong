c4accae3c7c4efe9900bb2f4123a5e18bed25a03
floating-sky <122412802+floating-sky@users.noreply.github.com>
2024-12-15 12:56:48 -0700

    volume settings finished

--- (GONE WRONG)/Assets/_Scripts/_Script_MainMenuController.cs
+++ (GONE WRONG)/Assets/_Scripts/_Script_MainMenuController.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
 
 public class _Script_MainMenuController : MonoBehaviour
 {
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.None;
+
+        masterSlider.value = PlayerPrefs.GetFloat("masterVolume", 1);
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume", 1);
+        sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume", 1);
+        mixer.SetFloat("Master", Mathf.Log10(masterSlider.value) * 20);
+        mixer.SetFloat("Music", Mathf.Log10(musicSlider.value) * 20);
+        mixer.SetFloat("SFX", Mathf.Log10(sfxSlider.value) * 20);
     }
 
     public void PlayGame()
@@ -23,5 +38,26 @@ public class _Script_MainMenuController : MonoBehaviour
     public void TitleScreen()
     {
         SceneManager.LoadScene("Main Menu");
+    }
+
+    public void SetMasterVolume()
+    {
+        float masterVolume = masterSlider.value;
+        mixer.SetFloat("Master", Mathf.Log10(masterVolume) * 20);
+        PlayerPrefs.SetFloat("masterVolume", masterSlider.value);
+    }
+
+    public void SetMusicVolume()
+    {
+        float musicVolume = musicSlider.value;
+        mixer.SetFloat("Music", Mathf.Log10(musicVolume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", musicSlider.value);
+    }
+
+    public void SetSFXVolume()
+    {
+        float sfxVolume = sfxSlider.value;
+        mixer.SetFloat("SFX", Mathf.Log10(sfxVolume) * 20);
+        PlayerPrefs.SetFloat("sfxVolume", sfxSlider.value);
     }
 }
